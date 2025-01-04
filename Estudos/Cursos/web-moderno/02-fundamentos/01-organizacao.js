729a30f63f7781e72a40738cda374344875a264e
Henrincode <spellctro@gmail.com>
2025-01-04 12:57:10 -0300

    Estudando

---
+++ Estudos/Cursos/web-moderno/02-fundamentos/01-organizacao.js
@@ -0,0 +1,11 @@
+// JavaScript é organizado em sentenças e blocos de códigos.
+
+console.log(
+    "Sentença de código")
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') // Padrão do curso sem ;
+    }
+}
