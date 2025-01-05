70e2255a44cc5fa13907121202f2bd78024d0709
Henrincode <spellctro@gmail.com>
2025-01-05 12:41:27 -0300

    Estudando

---
+++ Estudos/Cursos/web-moderno/02-fundamentos/03-variaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3;
+let b = 4;
+
+var a = 30; // var pode ser redeclarada
+b = 40;     // let não pode ser redeclarada
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+
+console.log(a, b);
+
+const c = 5;
+// c = 50; const não pode ser alterada
+
+console.log(c);
