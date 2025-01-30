be7fe43b966c982c8a6360eb7ad6e3967ae39534
Henrincode <spellctro@gmail.com>
2025-01-30 14:19:01 -0300

    Update 19-classeVsFactory.js

--- Estudos/Cursos/web-moderno/04-funcao/19-classeVsFactory.js
+++ Estudos/Cursos/web-moderno/04-funcao/19-classeVsFactory.js
@@ -4,13 +4,13 @@
 class Pessoa {
     constructor(nome) {
         this.nome = nome; // Atribuindo o nome ao objeto
-    }
+    };
 
     // Método para a pessoa se apresentar
     falar() {
         console.log(`Meu nome é ${this.nome}.`); // Exibindo o nome no console
-    }
-}
+    };
+};
 
 // Criando uma instância de Pessoa e chamando o método 'falar'
 const p1 = new Pessoa('João');
@@ -21,7 +21,7 @@ const criarPessoa = nome => {
     return {
         falar: () => console.log(`Meu nome é ${nome}.`) // A função 'falar' usa o nome fornecido
     };
-}
+};
 
 // Criando uma pessoa com a função factory e chamando o método 'falar'
 const p2 = criarPessoa('João');
