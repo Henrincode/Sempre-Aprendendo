582893552506df8ba7db9cb1065f60f0fb2acd59
Henrincode <spellctro@gmail.com>
2025-01-31 01:52:51 -0300

    Desafio 4

---
+++ Estudos/Cursos/web-moderno/04-funcao/exercicios/04.js
@@ -0,0 +1,10 @@
+// Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão destes dois valores.
+
+const divisao = (a, b) => console.log(`${a} / ${b} = ${a / b}. Resto = ${a % b}.`);
+
+divisao(4, 2);
+divisao(4, 1);
+divisao(4, 4);
+divisao(7, 2);
+divisao(7, 1);
+divisao(7, 4);
