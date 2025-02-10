bfb61b42568080452a74fa7dc2c0cdcfdfaeabf7
Henrincode <spellctro@gmail.com>
2025-02-10 10:07:50 -0300

    Desafio do professor

---
+++ Estudos/Cursos/web-moderno/04-funcao/exercicios/24.js
@@ -0,0 +1,7 @@
+/* Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while. */
+
+let vezes = 1;
+while(vezes <= 15){
+    console.log(`${vezes} Hello World`)
+    vezes++
+}
