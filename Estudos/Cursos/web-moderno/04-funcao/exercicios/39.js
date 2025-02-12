b94df53f587fa17e9063d6ec5aa5556e3e48851a
Henrincode <spellctro@gmail.com>
2025-02-12 14:46:27 -0300

    Desafio do professor

--- Estudos/Cursos/web-moderno/04-funcao/exercicios/39.js
+++ Estudos/Cursos/web-moderno/04-funcao/exercicios/39.js
@@ -2,8 +2,16 @@
 primeiro elemento do vetorA passe a ser o primeiro elemento do vetorB e vice versa e assim sucessivamente.
 Faça a troca sem utilizar uma variável auxiliar. */
 
+const trocaVetor = (vetorA, vetorB) => {
+    if (vetorA.length == vetorB.length) {
+        [vetorA, vetorB] = [vetorB, vetorA];
+        return `Vetor A = ${vetorA}. Vetor B = ${vetorB}.`
+    } else {
+        return "Vetores de tamanhos diferentes."
+    }
+}
 let vetorA = [1, 2, 3], vetorB = ["a", "b", "c"];
 
-[vetorA, vetorB] = [vetorB, vetorA];
-
-console.log(vetorA, vetorB);
+console.log(trocaVetor(vetorA, vetorB));
+vetorA = [1, 2];
+console.log(trocaVetor(vetorA, vetorB));
