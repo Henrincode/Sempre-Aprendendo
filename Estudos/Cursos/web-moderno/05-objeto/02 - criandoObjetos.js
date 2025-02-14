9a710c3f7496fefead68e2c30da5fcdc075c324c
Henrincode <spellctro@gmail.com>
2025-02-13 22:24:47 -0300

    Código depois de revisado (aula importante)

--- Estudos/Cursos/web-moderno/05-objeto/02 - criandoObjetos.js
+++ Estudos/Cursos/web-moderno/05-objeto/02 - criandoObjetos.js
@@ -1,43 +1,46 @@
-// Exemplos das diferentes formas de se criar um objeto
+// Exemplos das diferentes formas de se criar um objeto em JavaScript
 
-// usando a notação literal
-const obj1 = {};
+// Criando um objeto usando a notação literal
+const obj1 = {}; // Objeto vazio
 console.log(obj1);
 
-// Object em JS
-console.log(typeof Object, typeof new Object);
-const obj2 = new Object;
+// Criando um objeto com a função construtora Object
+console.log(typeof Object, typeof new Object()); // Object é uma função, new Object() cria um objeto
+const obj2 = new Object(); // Criando um objeto a partir de Object
 console.log(obj2);
 
-// Funções construtoras
-function Produto(nome, preco, desc) {
-    this.nome = nome;
-    this.getPrecoComDesconto = () => {
-        return preco * (1 - desc);
+// Criando objetos usando funções construtoras
+function Produto(nome, preco, desconto) {
+    this.nome = nome; // Atributo público
+    
+    // Método para calcular o preço com desconto
+    this.getPrecoComDesconto = function() {
+        return preco * (1 - desconto);
     };
-};
+}
 
 const p1 = new Produto('Caneta', 7.99, 0.15);
 const p2 = new Produto('Notebook', 2998.99, 0.25);
 console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto());
 
-// Função Factory
-function criarFuncionario(nome, salarioBase, faltas){
+// Criando objetos usando uma função Factory (Padrão Factory)
+function criarFuncionario(nome, salarioBase, faltas) {
     return {
         nome,
         salarioBase,
         faltas,
+        // Método para calcular o salário com base nas faltas
         getSalario() {
             return (salarioBase / 30) * (30 - faltas);
         }
     };
-};
+}
 
 const f1 = criarFuncionario('João', 7980, 4);
 const f2 = criarFuncionario('Maria', 11400, 1);
 console.log(f1.getSalario(), f2.getSalario());
 
-// Object.creat
-const filha = Object.create(null);
+// Criando um objeto usando Object.create
+const filha = Object.create(null); // Cria um objeto sem protótipo
 filha.nome = 'Ana';
 console.log(filha);
