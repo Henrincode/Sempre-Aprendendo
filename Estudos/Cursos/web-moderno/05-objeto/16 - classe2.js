2a4c2c21913d8cd966becdad6465c054f719573c
Henrincode <spellctro@gmail.com>
2025-02-27 11:55:46 -0300

    Adicionando comentários para melhor entender

--- Estudos/Cursos/web-moderno/05-objeto/16 - classe2.js
+++ Estudos/Cursos/web-moderno/05-objeto/16 - classe2.js
@@ -1,21 +1,30 @@
+// Classe Avo, que representa a classe base (ancestral)
 class Avo {
     constructor(sobrenome) {
+        // O sobrenome é atribuído ao objeto Avo
         this.sobrenome = sobrenome;
     };
 };
 
+// Classe Pai, que herda da classe Avo
 class Pai extends Avo {
-    constructor(sobrenome, profssao = 'Professor') {
+    constructor(sobrenome, profissao = 'Professor') {
+        // Chama o construtor da classe Avo para inicializar o sobrenome
         super(sobrenome);
-        this.profssao = profssao;
+        // Define a profissão do pai, com valor padrão 'Professor'
+        this.profissao = profissao;
     };
 };
 
+// Classe Filho, que herda da classe Pai
 class Filho extends Pai {
     constructor() {
+        // Chama o construtor da classe Pai, passando o sobrenome 'Silva'
         super('Silva');
     };
 };
 
-const filho = new Filho;
+// Criando uma instância da classe Filho
+const filho = new Filho();
+// Exibe no console as propriedades do objeto filho
 console.log(filho);
