d6a535646d7e83efdf66261c6bc5ea13894986cb
Henrincode <spellctro@gmail.com>
2025-03-06 10:54:17 -0300

    Update 15 - somentePareseIndicesPares.js

--- Estudos/Cursos/web-moderno/06 - array/exercicios/15 - somentePareseIndicesPares.js
+++ Estudos/Cursos/web-moderno/06 - array/exercicios/15 - somentePareseIndicesPares.js
@@ -1,7 +1,8 @@
-// Elabore uma função que receba um array de números e retorne um array que tenha todos os números que são
-// pares e que também tenham índices pares.
+// Elabore uma função que receba um array de números e retorne um array
+// que tenha todos os números que são pares e que também tenham índices pares.
 
-// Lembre-se que um número é par porque é divisível por 2, ou seja, o resto da divisão dele por 2 é zero.
+// Lembre-se que um número é par porque é divisível por 2,
+// ou seja, o resto da divisão dele por 2 é zero.
 
 // Exemplos:
 // receberSomenteOsParesDeIndicesPares([1, 2, 3, 4]) // retornará []
