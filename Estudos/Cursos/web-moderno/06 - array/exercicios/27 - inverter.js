cf7a3555cd848c841e262a2d2633cf985e979e66
Henrincode <spellctro@gmail.com>
2025-03-07 14:05:57 -0300

    Desafio do professor

---
+++ Estudos/Cursos/web-moderno/06 - array/exercicios/27 - inverter.js
@@ -0,0 +1,16 @@
+// Desenvolva uma função que receba um objeto como parâmetro e retorne um outro
+// objeto que corresponda ao objeto recebido como parâmetro,
+// porém com as posições das chaves e valores invertidas, conforme exemplo a seguir:
+
+// Exemplo:
+
+// inverter({ a: 1, b: 2, c: 3}) // retornará { 1: "a", 2: "b", 3: "c"}
+
+function inverter(objeto) {
+    const paresDeChaveValorInvertidos = Object.entries(objeto)
+        .map(parChaveValor => parChaveValor.reverse());
+    const novoObj = Object.fromEntries(paresDeChaveValorInvertidos);
+    console.log(novoObj);
+}
+
+inverter({ a: 1, b: 2, c: 3}); // retornará { 1: "a", 2: "b", 3: "c"}
