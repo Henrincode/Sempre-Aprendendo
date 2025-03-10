e91f610d93ec6fab22be6a9c2cf9fe4ade45c762
Henrincode <spellctro@gmail.com>
2025-03-10 18:35:33 -0300

    Diferença entre module.exports, exports e this em Node.js
    Aprendi um pouco mais sobre como module.exports, exports e this funcionam em Node.js. Entendi que todos apontam para o mesmo objeto inicialmente, mas ao reatribuir exports, ele deixa de referenciar module.exports. Também aprendi que apenas module.exports define o que será exportado do módulo.

---
+++ Estudos/Cursos/web-moderno/07 - node/04-exportar.js
@@ -0,0 +1,60 @@
+// module.exports inicia como um objeto vazio por padrão
+console.log(module.exports) 
+// Saída: {}
+
+// No início, 'this', 'exports' e 'module.exports' apontam para o mesmo objeto
+console.log(module.exports === this) 
+// Saída: true
+
+console.log(module.exports === exports) 
+// Saída: true
+
+// Adicionando propriedades ao objeto compartilhado
+this.a = 1
+exports.b = 2
+module.exports.c = 3
+
+console.log(module.exports) 
+// Saída: { a: 1, b: 2, c: 3 }
+
+// 'this' e 'exports' ainda referenciam o mesmo objeto
+console.log(module.exports === this) 
+// Saída: true
+
+console.log(module.exports === exports) 
+// Saída: true
+
+// Aqui, 'exports' é reatribuído para 'null', mas isso NÃO afeta 'module.exports'
+exports = null 
+
+console.log(exports) 
+// Saída: null
+
+// Após reatribuir 'exports', ele não aponta mais para 'module.exports'
+console.log(module.exports === this) 
+// Saída: true
+
+console.log(module.exports === exports) 
+// Saída: false
+
+console.log(module.exports) 
+// Saída: { a: 1, b: 2, c: 3 } (continua inalterado)
+
+// Agora 'exports' recebe um novo objeto
+exports = { nome: 'Teste' }
+
+console.log(module.exports) 
+// Saída: { a: 1, b: 2, c: 3 } (permanece o mesmo)
+
+// Aqui, 'module.exports' é redefinido completamente
+module.exports = { publico: true }
+
+console.log(module.exports) 
+// Saída: { publico: true }
+
+// Agora 'this' e 'exports' não apontam mais para 'module.exports'
+console.log(module.exports === this) 
+// Saída: false
+
+console.log(module.exports === exports) 
+// Saída: false
