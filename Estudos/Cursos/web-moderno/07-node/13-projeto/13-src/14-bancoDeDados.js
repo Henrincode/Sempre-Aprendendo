c21366574503de25661ffac1e02efe1271183510
Henrincode <spellctro@gmail.com>
2025-03-14 08:15:47 -0300

    Criando uma API Simples com Node.js e Express
    Aprendi um pouco mais sobre como criar uma API básica usando Node.js e o framework Express. Descobri como configurar rotas GET e POST para listar e adicionar produtos, além de usar um objeto em memória como banco de dados simples. Entendi como o Express gerencia requisições e respostas e como exportar funções entre arquivos com module.exports.

---
+++ Estudos/Cursos/web-moderno/07-node/13-projeto/13-src/14-bancoDeDados.js
@@ -0,0 +1,27 @@
+// Objeto para gerar IDs sequenciais
+const sequence = {
+    _id: 1, // Valor inicial do ID
+    get id() { return this._id++ } // Getter que retorna o ID atual e incrementa para o próximo
+}
+
+const produtos = {} // Objeto que armazena os produtos como um "banco de dados" em memória
+
+// Função para salvar um produto no "banco de dados"
+function salvarProduto(produto) {
+    if (!produto.id) produto.id = sequence.id // Atribui um ID ao produto se ele não tiver um
+    produtos[produto.id] = produto // Armazena o produto no objeto produtos usando o ID como chave
+    return produto // Retorna o produto salvo
+}
+
+// Função para buscar um produto pelo ID
+function getProduto(id) {
+    return produtos[id] || {} // Retorna o produto correspondente ao ID ou um objeto vazio se não existir
+}
+
+// Função para listar todos os produtos
+function getProdutos() {
+    return Object.values(produtos) // Converte o objeto produtos em um array com todos os valores
+}
+
+// Exporta as funções para uso em outros arquivos
+module.exports = { salvarProduto, getProduto, getProdutos }
