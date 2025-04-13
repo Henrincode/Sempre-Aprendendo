4ef575e2d18c6f8f4dd706b5852197066cdc16e1
Henrincode <spellctro@gmail.com>
2025-04-13 13:58:59 -0300

    desafio: resolvendo pelo menos um desafio por dia

---
+++ Estudos/exercicios/2025-04-13/01-contarPalavras.js
@@ -0,0 +1,6 @@
+// Criar uma função que receba uma string e conte quantas palavars tem nela
+
+const contarPalavras = p => p.split(" ").length;
+
+console.log(contarPalavras("Sou uma frase")); // retornará 3
+console.log(contarPalavras("Me divirto aprendendo a programar")); // retornará 5
