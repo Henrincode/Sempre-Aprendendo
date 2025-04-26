e73e3f0aab6167a034c9847cd5ce1c3538fd00cd
Henrincode <spellctro@gmail.com>
2025-04-26 11:15:31 -0300

    desafios: inserindo link para ir até a página do desafio

--- Estudos/exercicios/codewars/01-stringEndsWith.js
+++ Estudos/exercicios/codewars/01-stringEndsWith.js
@@ -1,12 +1,8 @@
-// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
-
-// Examples:
-
-// solution('abc', 'bc') // returns true
-// solution('abc', 'd') // returns false
+// Link do desafio: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
 
 const solution = (str, ending) => str.endsWith(ending);
-console.log(solution("abc", "bc"));
-console.log(solution("abc", "d"));
-console.log(solution("abcde", "cde"));
-console.log(solution("abcde", "abc"));
+
+console.log(solution("abc", "bc")); // true
+console.log(solution("abc", "d")); // false
+console.log(solution("abcde", "cde")); // true
+console.log(solution("abcde", "abc")); // false
