923d0f06f02b8f2bbddcc3b7740b7e647a914999
Henrincode <spellctro@gmail.com>
2025-05-11 19:54:30 -0300

    pasta web-moderno transformada em repositório

--- Estudos/Cursos/web-moderno/07-node/04-exportarCliente.js
+++
@@ -1,2 +0,0 @@
-const exportar = require('./04-exportar')
-console.log(exportar)
--- Estudos/Cursos/web-moderno/07-node/06-instanciaUnica.js
+++
@@ -1,8 +0,0 @@
-// Exporta um único objeto que será compartilhado entre todos os requires
-// O Node.js armazena este módulo em cache, criando uma instância única
-module.exports = {
-    valor: 1,
-    inc() {
-        this.valor++; // Incrementa o valor da instância atual
-    }
-};
