60c3400f7cda709ef9a9a16af431129b317e9071
Henrique inCode <henrincode@gmail.com>
2025-05-12 21:55:34 -0300

    arquivos de Sempre-Aprendendo movidos para CodeWars

--- Estudos/exercicios/codewars/03-arrayDiff.js
+++
@@ -1,8 +0,0 @@
-// Link do desafio: https://www.codewars.com/kata/523f5d21c841566fde000009
-
-const arrayDiff = (a, b) => a.filter(a => !b.includes(a));
-
-console.log(arrayDiff([1, 2, 2], [1])); // [2,2]
-console.log(arrayDiff([1, 2, 2], [])); // [1,2,2]
-console.log(arrayDiff([], [1, 2])); // []
-console.log(arrayDiff([1, 2], [1])); // [2]
